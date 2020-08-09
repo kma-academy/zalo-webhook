a36739430e8b857966cff2624ba1d6a091995d71
Cuong Tran <clonebmn2itt@gmail.com>
2020-08-09 11:19:03 +0700

    feat: First commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require("express");
+const { dump } = require('dumper.js');
+const app = express();
+app.use(express.json());
+app.set('port', process.env.PORT);
+
+app.get('/', function (req, res) {
+    const { params, query, headers } = req;
+    res.send({ params, query, headers });
+});
+app.get("/callback", function (req, res) {
+    const { access_token, oaId } = req.query;
+    res.status(200).send(access_token);
+})
+
+app.get("/webhook", function (req, res) {
+    res.status(200).send("OK");
+});
+app.post("/webhook", function (req, res) {
+    dump(req.body);
+    res.status(200).send("OK");
+});
+
+
+app.listen(app.get('port'), function () {
+    console.log("Listening in port %d", app.get('port'));
+});
