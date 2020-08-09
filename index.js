a36fc3c1c95aa2fbea3f4b54b535d8fa209a1ef7
Cuong Tran <clonebmn2itt@gmail.com>
2020-08-09 11:42:59 +0700

    feat: simple echo bot

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
-const express = require("express");
+const express = require('express');
 const { dump } = require('dumper.js');
+const axios = require('axios');
 const app = express();
 app.use(express.json());
 app.set('port', process.env.PORT);
@@ -21,11 +22,41 @@ app.get("/webhook", function (req, res) {
     res.status(200).send("OK");
 });
 app.post("/webhook", function (req, res) {
-    dump(req.body);
+    const event = req.body;
+    hookProcess(event);
     res.status(200).send("OK");
 });
-
-
+function hookProcess(event) {
+    dump(event);
+    switch (event.event_name) {
+        case "user_send_text":
+            textProcess(event);
+            break;
+        default:
+            break;
+    }
+}
+function textProcess(event) {
+    const senderId = event.sender.id;
+    const messageText = event.message.text;
+    console.log(`User ${senderId} send text "${messageText}"`);
+    axios({
+        method: "POST",
+        url: "https://openapi.zalo.me/v2.0/oa/message",
+        params: {
+            access_token: app.get("token")
+        },
+        data: {
+            recipient: {
+                user_id: senderId
+            },
+            message: {
+                text: messageText
+            }
+        }
+    })
+        .then(e => console.log(e.data))
+}
 app.listen(app.get('port'), function () {
     console.log("Listening in port %d", app.get('port'));
 });
